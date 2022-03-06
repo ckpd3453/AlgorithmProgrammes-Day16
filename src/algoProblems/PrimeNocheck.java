776df962bf97f5cbbf991d4daea236d20cf43dc2
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-06 22:20:05 +0530

    Added UC-8 extend Prime No to check palindrom of Algorithm Problems

--- src/algoProblems/PrimeNocheck.java
+++ src/algoProblems/PrimeNocheck.java
@@ -4,35 +4,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PrimeNo {
+public class PrimeNocheck {
 
 	/**
-	 * PROCDURE:-
+	 * PROCDURE:- 
 	 * ========= 
-	 * Take a range of 0 - 1000 Numbers and find the Prime numbers in that range. 
-	 * ========================================================== 
-	 * 1.created a Arraylist 
+	 * Extend the previous program to find the prime numbers that are Anagram and Palindrome.
+	 * ======================================================================================= 
+	 * 1.created two Arraylist 
 	 * 2.giving input start range and end range 
-	 * 3.printing a range 
+	 * 3.printing a range
 	 * 4.created method to check the number is prime or not 
-	 * 5.Method to print prime number 
+	 * 5.created method to check whether given prime no. is palindrom and Anagram or not. 
+	 * 6.Method to print prime number 
+	 * 
 	 * ==========================================================
 	 */
 
 	// 1.created a Arraylist
 	static ArrayList<Integer> arrayList = new ArrayList<>();
+	static ArrayList<Integer> arrayList2 = new ArrayList<>();
 
 	public static void main(String[] args) {
-		
+
 		// 2.giving input start range and end range
 		final int startRange = 0;
 		final int endRange = 1000;
-		
+
 		// 3.printing a range
 		primePrint(startRange, endRange);
 	}
 
-	/**
+	/*
 	 * 4. created method to check the number is prime or not
 	 * 
 	 * @param num-Number to be checked
@@ -46,7 +49,15 @@ public class PrimeNo {
 
 			if (num % i == 0) {
 				if (i == num) {
-					return true;
+
+					/*
+					 *After getting prime no. calling isPalindrom method to check whether number
+					 * is palindrom or not and storing it into another arraylist
+					 */
+					isPalindrom(num);
+					if (isPalindrom(num)) {
+						arrayList2.add(num);
+					}
 				} else {
 					return false;
 				}
@@ -57,8 +68,25 @@ public class PrimeNo {
 		return true;
 	}
 
-	/**
-	 * 5.Method to print prime number
+	//5. Create a isPlaindrom method to check whether the prime no. is palindrom or not
+	public static boolean isPalindrom(Integer num) {
+		int r, sum = 0;
+
+		int orn = num;
+		while (num > 0) {
+			r = num % 10;
+			sum = (sum * 10) + r;
+			num = num / 10;
+		}
+		if (orn == sum) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/*
+	 * 6.Method to print prime number
 	 * 
 	 * @param startRange-1
 	 * @param endRange-1000
@@ -70,6 +98,8 @@ public class PrimeNo {
 				arrayList.add(range);
 			}
 		}
-		System.out.println(arrayList);
+		System.out.println("Prime no : " + arrayList);
+		System.out.println("\n\nPalindrom Prime no : " + arrayList2);
+		System.out.println("\nIf a number is Palindrom then it is a Anagram number.");
 	}
 }
